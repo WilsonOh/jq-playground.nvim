ac2577c3fe40264cbe2f16bbdb7a27551d597873
WilsonOh <oh.wilson123@gmail.com>
2025-10-03 17:23:25 +0800

    feat: disallow running on non-json files

--- lua/jq-playground/init.lua
+++ lua/jq-playground/init.lua
@@ -6,6 +6,12 @@ function M.setup(opts)
   confmod.config = vim.tbl_deep_extend("force", confmod.default_config, opts or {})
 
   vim.api.nvim_create_user_command("JqPlaygroundToggle", function()
+    local curbuf = vim.api.nvim_get_current_buf()
+    local filetype = vim.api.nvim_get_option_value("filetype", { buf = curbuf })
+    if filetype ~= "json" then
+      vim.notify_once("jq playground not availble for non-json files", vim.log.levels.ERROR)
+      return
+    end
     require("jq-playground.playground").toggle_playground()
   end, {
     desc = "Toggle jq playground",
